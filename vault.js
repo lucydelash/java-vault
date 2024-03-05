e20092be07f2ba45ef3da9ea3123eb6bf35a6cab
lucydelash <lucyrudd614@gmail.com>
2024-03-05 13:04:47 -0500

    alinked index.html & vault.js. added code to both pages as well

---
+++ vault.js
@@ -0,0 +1,20 @@
+/* psuedocode
+1. "you've received this message..." alert
+2. first code using addition (5+5=10)
+3. second code using multiplication (4*10=40)
+4. third code using subtraction (40-1=39)
+5. failure or success alert
+*/
+
+// selection screen
+
+window.alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+
+// first code = 10
+let code1 = 5 + 5;
+
+// second code = 40
+let code2 = 4 * code1;
+
+// third code = 39
+let code3 = code2 - 1;
